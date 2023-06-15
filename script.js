4f883d2ab9c08f001a152c674b7d3e1699414c4f
chabandou <133357543+chabandou@users.noreply.github.com>
2023-06-15 12:41:25 +0100

    Update script.js
    Feedback updates

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 let allRatings = document.querySelector('form')
-let selectedRating = 0;
 const submitButton = document.querySelector('#submitBtn');
 let ratingDisplay = document.querySelector('#ratingDisplay');
 const thankYouSec = document.querySelector('#thankYouSec');
 const mainSec = document.querySelector('#mainSec')
 
-// thankYouSec.style.display = "none"
 
 
-allRatings.addEventListener('click', function() {
-    selectedRating = document.querySelector('input[name="rating"]:checked');
-})
-
-
-
-
-
-submitButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (selectedRating !== 0) {
-        ratingDisplay.innerText = selectedRating.value;
-        // mainSec.style.display = "none"
-        // thankYouSec.style.display = "block"
-        mainSec.classList.toggle("display")
-        thankYouSec.classList.toggle("display")
+function submit() {
+    if (allRatings.rating.value) {
+        ratingDisplay.innerText = allRatings.rating.value;
+        mainSec.classList.toggle("display");
+        thankYouSec.classList.toggle("display");
     } else {
 
     }
-})
+}
 
+allRatings.addEventListener('submit', function(e) {
+    e.preventDefault();
+    submit()
+})
